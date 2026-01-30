2986ab8b1e71fcea3c17ed96a5c1bfa404bb53f6
Stoaties <corentin.gouanvic@gmail.com>
2026-01-29 23:22:53 -0500

    Added changes to test bot

--- database.sql
+++ database.sql
@@ -8,3 +8,10 @@ CREATE TABLE users (
     roles ENUM('admin', 'operator', 'user') DEFAULT 'user'
     likes INTEGER DEFAULT 0
 );
+
+CREATE TABLE memos(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
